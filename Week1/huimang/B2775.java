518cf1ceb2eb1f02dab2a06f71679d31d04ee238
송영재 <thddudwo125@gmail.com>
2025-03-06 17:48:08 +0900

    [Week1] Add B2775 Solutions

--- Week1/huimang/B2775.java
+++ Week1/huimang/B2775.java
@@ -1,8 +1,44 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class B2775 {
-    public static void main(String[] args) throws IOException {
-        
+
+    static int[][] memo;
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t<T; t++) {
+            
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            memo = new int[k+1][n+1];
+
+            for(int i = 1; i<=n; i++) {
+                memo[0][i] = i;
+            }
+
+            for(int i = 1; i<=k; i++) {
+                for(int j = 1; j<=n; j++) {
+                    memo[i][j] = memo[i][j-1] + memo[i-1][j];
+                }
+            }
+            
+            System.out.println(memo[k][n]);
+        }
+
+
+
     }
+
+
 }
